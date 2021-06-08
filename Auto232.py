5aaee55e37f022a4748a5a9fa2a31080a0687e19
AndreLeEEEEE <72272834+AndreLeEEEEE@users.noreply.github.com>
2021-06-08 11:05:16 -0600

    Add files via upload

--- Auto232.py
+++ Auto232.py
@@ -1,4 +1,24 @@
-with open("BeltMovement", "r") as file:
-    Lines = file.readlines()
-    for line in Lines:
-        print(line)
+import openpyxl
+import re
+
+def readFile(logs):
+    with open("BeltMovement", "r") as file:
+        Lines = file.readlines()
+        temp = ""
+        for line in Lines:
+            if (line == "\n") or (line.__contains__("*")):
+                pass
+            elif line.__contains__("Termite log"):
+                logs.append([line[0:-1]])
+            elif len(line[0:-1]) == 1:
+                temp = line[0:-1]
+            elif temp:
+                logs[-1].append(line.split(" ", 1)[0] + " " + temp)
+                logs[-1].append(line.split(" ", 1)[1][0:-1])
+                temp = ""
+            else:
+                logs[-1].append(line[0:-1])
+
+def main():
+    logs = []
+    readFile(logs)
